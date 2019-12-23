46b0e76f96096269d561e1d937884181cb287458
Jui-Nan Lin <jnlinn@gmail.com>
2019-12-23 19:03:16 +0800

    refactor(App): use computed variable

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       <b-navbar-brand href="#">Fastdrop</b-navbar-brand>
       <b-nav-text>與附近的人快速分享檔案</b-nav-text>
     </b-navbar>
-    <b-alert show variant="primary" type="dark" v-if="!isReady()">為了找到附近的人，請允許定位與推播</b-alert>
-    <Permission v-on:hasGeolocation="hasGeolocation" v-on:hasNotification="hasNotification" v-if="!isReady()"/>
-    <UploadFile v-if="isReady()" v-bind:id="getId()"/>
+    <b-alert show variant="primary" type="dark" v-if="!isReady">為了找到附近的人，請允許定位與推播</b-alert>
+    <Permission v-on:hasGeolocation="hasGeolocation" v-on:hasNotification="hasNotification" v-if="!isReady"/>
+    <UploadFile v-if="isReady" v-bind:id="getId()"/>
   </div>
 </template>
 
@@ -24,6 +24,11 @@ export default {
       notification: false
     }
   },
+  computed: {
+    isReady: function () {
+      return this.notification && this.geolocation
+    }
+  },
   components: {
     Permission,
     UploadFile
@@ -34,9 +39,7 @@ export default {
     },
     hasGeolocation () {
       this.geolocation = true
-    },
-    isReady () {
-      return this.notification && this.geolocation
+      this.pos = pos
     },
     getId () {
       return randomWords() + Math.round(Math.random() * 1000)
