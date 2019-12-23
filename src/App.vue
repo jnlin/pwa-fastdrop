0c2f83379163ae5c7cff0c1ee9a9e930eb266f65
Jui-Nan Lin <jnlinn@gmail.com>
2019-12-23 16:07:12 +0800

    feat(App): import App.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-navbar-brand href="#">Fastdrop</b-navbar-brand>
+      <b-nav-text>與附近的人快速分享檔案</b-nav-text>
+    </b-navbar>
+    <b-alert show variant="primary" type="dark" v-if="!isReady()">為了找到附近的人，請允許定位與推播</b-alert>
+    <Permission v-on:hasGeolocation="hasGeolocation" v-on:hasNotification="hasNotification" v-if="!isReady()"/>
+    <UploadFile v-if="isReady()" v-bind:id="getId()"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+var randomWords = require('random-words');
+
+import Permission from './components/Permission'
+import UploadFile from './components/UploadFile'
 
 export default {
-  name: 'app',
+  name: 'App',
+  data: () => {
+    return {
+      geolocation: false,
+      notification: false
+    }
+  },
   components: {
-    HelloWorld
+    Permission,
+    UploadFile
+  },
+  methods: {
+    hasNotification () {
+      this.notification = true
+    },
+    hasGeolocation () {
+      this.geolocation = true
+    },
+    isReady () {
+      return this.notification && this.geolocation
+    },
+    getId () {
+      return randomWords() + Math.round(Math.random() * 1000)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
